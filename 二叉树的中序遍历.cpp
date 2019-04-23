0f45a711f33f0e0e4c7f5d192aabc6b0e66eba7c
ssh1222 <2960074137@qq.com>
2019-04-23 23:22:20 +0800

    迭代的思想

---
+++ 二叉树的中序遍历.cpp
@@ -0,0 +1,42 @@
+//给定一个二叉树，返回它的中序 遍历。
+//示例 :
+//输入 : [1, null, 2, 3]
+//	1
+//	\
+//	2
+//	/
+//	3
+//
+//输出:[1, 3, 2]
+
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root)
+	{
+		vector<int> vet;
+		stack<TreeNode*> st;
+		TreeNode *cur = root;
+		while (cur || !st.empty()){
+			while (cur){
+				st.push(cur);
+				cur = cur->left;
+			}
+
+			cur = st.top();
+			vet.push_back(cur->val);
+			st.pop();
+			cur = cur->right;
+		}
+
+		return vet;
+	}
+};
